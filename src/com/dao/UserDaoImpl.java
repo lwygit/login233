0db0378726350ca765d423eb216d05554cbc0eb5
lwygit <jyh120@outlook.com>
2020-07-01 12:02:09 +0800

    使用c3p0链接池替换了原先连接方法。

--- src/com/dao/UserDaoImpl.java
+++ src/com/dao/UserDaoImpl.java
@@ -6,29 +6,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.entity.User;
 import com.util.DBconn;
 import com.util.C3P0Util;
+
 public class UserDaoImpl implements UserDao {
 
     public boolean register(User user) {
         boolean flag = false;
-        Connection conn=null;
-
+        Connection conn = null;
         PreparedStatement ps = null;
         conn = C3P0Util.getConnection();
-        System.out.println("0101");
         try {
             ps = conn.prepareStatement("insert into user(name,password,sex,home,info) " +
                     "values('" + user.getName() + "','" + user.getPwd() + "','" + user.getSex() + "','" + user.getHome() + "','" + user.getInfo() + "')");
             ps.executeUpdate();
-            flag=true;
-            System.out.println("添加操作执行成功！");
+            flag = true;
         } catch (SQLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-            System.out.println("添加操作执行失败！");
-            flag=false;
+            flag = false;
         } finally {
             C3P0Util.release(conn, ps, null);
         }
@@ -47,30 +45,54 @@ public class UserDaoImpl implements UserDao {
 //        return flag;
 //    }
 
-
-
     public boolean login(String name, String password) {
         boolean flag = false;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        conn = C3P0Util.getConnection();
         try {
-            DBconn.init();
-            ResultSet rs = DBconn.selectSql("select * from user where name='" + name + "' and password='" + password + "'");
+            ps = conn.prepareStatement("Select * from user where name='" + name + "' and password='" + password + "'");
+            rs = ps.executeQuery();
             while (rs.next()) {
                 if (rs.getString("name").equals(name) && rs.getString("password").equals(password)) {
                     flag = true;
                 }
             }
-            DBconn.closeConn();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            C3P0Util.release(conn, ps, rs);
         }
         return flag;
     }
 
+//    public boolean login(String name, String password) {
+//        boolean flag = false;
+//        try {
+//            DBconn.init();
+//            ResultSet rs = DBconn.selectSql("select * from user where name='" + name + "' and password='" + password + "'");
+//            while (rs.next()) {
+//                if (rs.getString("name").equals(name) && rs.getString("password").equals(password)) {
+//                    flag = true;
+//                }
+//            }
+//            DBconn.closeConn();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return flag;
+//    }
+
     public List<User> getUserAll() {
-        List<User> list = new ArrayList<User>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        conn = C3P0Util.getConnection();
         try {
-            DBconn.init();
-            ResultSet rs = DBconn.selectSql("select * from user");
+            ps = conn.prepareStatement("Select * from user");
+            rs = ps.executeQuery();
+            List<User> list = new ArrayList<User>();
             while (rs.next()) {
                 User user = new User();
                 user.setId(rs.getInt("id"));
@@ -81,55 +103,119 @@ public class UserDaoImpl implements UserDao {
                 user.setInfo(rs.getString("info"));
                 list.add(user);
             }
-            DBconn.closeConn();
             return list;
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            C3P0Util.release(conn, ps, rs);
         }
         return null;
     }
 
-    public boolean update(int id, String name, String password, String sex, String home, String info) {
-        boolean flag = false;
-        DBconn.init();
-        String sql = "update user set name ='" + name
-                + "' , password ='" + password
-                + "' , sex ='" + sex
-                + "' , home ='" + home
-                + "' , info ='" + info + "' where id = " + id;
-        int i = DBconn.addUpdDel(sql);
-        if (i > 0) {
-            flag = true;
-        }
-        DBconn.closeConn();
-        return flag;
-    }
+//    public List<User> getUserAll() {
+//        List<User> list = new ArrayList<User>();
+//        try {
+//            DBconn.init();
+//            ResultSet rs = DBconn.selectSql("select * from user");
+//            while (rs.next()) {
+//                User user = new User();
+//                user.setId(rs.getInt("id"));
+//                user.setName(rs.getString("name"));
+//                user.setPwd(rs.getString("password"));
+//                user.setSex(rs.getString("sex"));
+//                user.setHome(rs.getString("home"));
+//                user.setInfo(rs.getString("info"));
+//                list.add(user);
+//            }
+//            DBconn.closeConn();
+//            return list;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+//    public boolean update(int id, String name, String password, String sex, String home, String info) {
+//        boolean flag = false;
+//        DBconn.init();
+//        String sql = "update user set name ='" + name
+//                + "' , password ='" + password
+//                + "' , sex ='" + sex
+//                + "' , home ='" + home
+//                + "' , info ='" + info + "' where id = " + id;
+//        int i = DBconn.addUpdDel(sql);
+//        if (i > 0) {
+//            flag = true;
+//        }
+//        DBconn.closeConn();
+//        return flag;
+//    }
+
     public boolean update(User user) {
         boolean flag = false;
-        DBconn.init();
-        String sql = "update user set name ='" + user.getName()
-                + "' , password ='" + user.getPwd()
-                + "' , sex ='" + user.getSex()
-                + "' , home ='" + user.getHome()
-                + "' , info ='" + user.getInfo() + "' where id = " + user.getId();
-        int i = DBconn.addUpdDel(sql);
-        if (i > 0) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        conn = C3P0Util.getConnection();
+        try {
+            ps = conn.prepareStatement("update user set name ='" + user.getName()
+                    + "' , password ='" + user.getPwd()
+                    + "' , sex ='" + user.getSex()
+                    + "' , home ='" + user.getHome()
+                    + "' , info ='" + user.getInfo() + "' where id = " + user.getId());
+            ps.executeUpdate();
             flag = true;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+            C3P0Util.release(conn, ps, null);
         }
-        DBconn.closeConn();
         return flag;
     }
 
+//    public boolean update(User user) {
+//        boolean flag = false;
+//        DBconn.init();
+//        String sql = "update user set name ='" + user.getName()
+//                + "' , password ='" + user.getPwd()
+//                + "' , sex ='" + user.getSex()
+//                + "' , home ='" + user.getHome()
+//                + "' , info ='" + user.getInfo() + "' where id = " + user.getId();
+//        int i = DBconn.addUpdDel(sql);
+//        if (i > 0) {
+//            flag = true;
+//        }
+//        DBconn.closeConn();
+//        return flag;
+//    }
+
+
     public boolean delete(int id) {
         boolean flag = false;
-        DBconn.init();
-        String sql = "delete  from user where id=" + id;
-        int i = DBconn.addUpdDel(sql);
-        if (i > 0) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        conn = C3P0Util.getConnection();
+        try {
+            ps = conn.prepareStatement("delete  from user where id=" + id);
+            ps.executeUpdate();
             flag = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            C3P0Util.release(conn, ps, null);
         }
-        DBconn.closeConn();
         return flag;
     }
+//    public boolean delete(int id) {
+//        boolean flag = false;
+//        DBconn.init();
+//        String sql = "delete  from user where id=" + id;
+//        int i = DBconn.addUpdDel(sql);
+//        if (i > 0) {
+//            flag = true;
+//        }
+//        DBconn.closeConn();
+//        return flag;
+//    }
 
 }
